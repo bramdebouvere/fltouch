0b5b0379c56ab6fa7ad6d1fb32a0ff10b78ece70
rtosman <rennieallen@gmail.com>
2023-12-07 18:11:22 -0800

    - Fixed RgbToHsv - Created a Unit test - Configured the color map for the new RgbToHsv output

--- mcu_colors.py
+++ mcu_colors.py
@@ -16,20 +16,14 @@ def GetMcuColor(intValue):
     """ Get the MCU Screen Color code from an Int value (FL Studio Color Value) """
     c_hsv = IntToHsv(intValue)
     
-    if c_hsv[Hue] < 0:
-        thue = c_hsv[Hue]*-1
-        thue = (255-thue)
-        c_hsv = (thue, c_hsv[Saturation], c_hsv[Value])
-
     # Define color mapping table
     color_ranges = [
-        (0, 28, ScreenColorRed),
-        (29, 50, ScreenColorYellow),
-        (51, 117, ScreenColorGreen),
-        (118, 140, ScreenColorCyan),
-        (141, 190, ScreenColorBlue),
-        (191, 236, ScreenColorPurple),
-        (237, 255, ScreenColorRed)
+        (0, 79, ScreenColorYellow),
+        (80, 119, ScreenColorGreen),
+        (120, 149, ScreenColorCyan),
+        (150, 169, ScreenColorBlue),
+        (200, 350, ScreenColorPurple),
+        (351, 399, ScreenColorRed)
     ]
 
     if c_hsv[Value] > 30 and c_hsv[Saturation] > 40:
@@ -50,57 +44,27 @@ def IntToHsv(intValue):
     """ Convert an FL Studio Color Value (Int) to HSV """
     return(RgbToHsv(IntToRGB(intValue)))
 
-def IntToRGB(intValue):
-    """ Convert an FL Studio Color Value (Int) to RGB """
-    blue = intValue & 255
-    green = (intValue >> 8) & 255
-    red = (intValue >> 16) & 255
-    return (red, green, blue)
-
-    """
-    Converts an RGB color value to HSV.
-
-    This function transforms a color value from the RGB (Red, Green, Blue) color space 
-    to the HSV (Hue, Saturation, Value) color space. The input RGB values are 
-    expected to be in the range of 0 to 255. The function normalizes these values to 
-    the range 0-1 for internal calculations and then scales the resulting HSV values 
-    back to the 0-255 range.
-
-    Parameters:
-    rgb (tuple): A tuple of three integers representing the Red, Green, and Blue 
-                 components of the color. Each value should be in the range 0-255.
-
-    Returns:
-    tuple: A tuple of three integers representing the Hue, Saturation, and Value 
-           components of the color in the HSV color space. Hue is scaled to fit 
-           within 0-255 (originally it is defined in degrees from 0-360), and 
-           both Saturation and Value are in the range 0-255.
-
-    The Hue (H) calculation is done based on which RGB component is the maximum.
-    If there's no dominant color (i.e., all RGB values are equal), Hue is set to zero.
-    Saturation (S) is computed as the ratio of the difference between the maximum and
-    minimum RGB values to the maximum RGB value. If the maximum RGB value is zero,
-    Saturation is set to zero to avoid division by zero. Value (V) is simply the maximum
-    of the RGB components.
-    """
 def RgbToHsv(rgb):
-    """ Converts an RGB tuple (value range from 0 to 255) to an HSV tuple """
     R, G, B = rgb
+    R, G, B = R / 255.0, G / 255.0, B / 255.0
 
-    max_rgb = max(rgb)  # R = 255
-    min_rgb = min(rgb)  # B = 20
-    delta = int(max_rgb - min_rgb) # 255-20 = 235
+    max_rgb = max(R, G, B)
+    min_rgb = min(R, G, B)
+    delta = max_rgb - min_rgb
 
-    V = max_rgb  # Value 255
-    S = 0 if V == 0 else (255 * delta) // V  # Saturation 235/255 S = 0.922
+    V = max_rgb  # Value
+    S = 0 if max_rgb == 0 else (delta / max_rgb)  # Saturation
 
-    if S == 0:
-        H = 0
+    if delta == 0:
+         H = 0  # Hue
     elif max_rgb == R:
-        H = int(0 + 43*(G - B)//delta)
+        H = 60 * (((G - B) / delta) % 6)
     elif max_rgb == G:
-        H = int(85 + 43*(B - R)//delta)
-    else:
-        H = int(171 + 43*(R - G)//delta)
+        H = 85 + 43 * (B-R)/delta
+    elif max_rgb == B:
+        H = 171 + 43 * (R-G)/delta
 
-    return (H, S, V)
+    S = int(S * 255)
+    V = int(V * 255)
+
+    return (int(H), int(S), int(V))
