5a0b9e1c5010dfc49abc5e49ea97a894b92638bf
bramdebouvere <bram.debouvere@gmail.com>
2026-06-14 00:46:20 +0200

    Implement add marker and link channel buttons

--- behaviors/track_marker_buttons_behavior.py
+++ behaviors/track_marker_buttons_behavior.py
@@ -1,5 +1,7 @@
 import device
 import midi
+import transport
+import mixer
 from behaviors.mcu_base_behavior import McuBaseBehavior
 from device_hal import mcu_buttons
 from utilities.button_manager import ButtonManager
@@ -26,6 +28,21 @@ class TrackMarkerBehavior(McuBaseBehavior):
                         ButtonManager.JogSource = 0
                 else:
                     ButtonManager.JogSource = btn
+
+            elif btn == mcu_buttons.AddMarker:
+                if event.data2 > 0:
+                    transport.globalTransport(
+                        midi.FPT_AddMarker + int(ButtonManager.ShiftPressed),
+                        2,
+                        event.pmeFlags,
+                    )
+
+            elif btn == mcu_buttons.LinkChannel:
+                if event.data2 > 0:
+                    if ButtonManager.ShiftPressed:
+                        mixer.linkTrackToChannel(midi.ROUTE_StartingFromThis)
+                    else:
+                        mixer.linkTrackToChannel(midi.ROUTE_ToThis)
 
             device.directFeedback(event)
             event.handled = True
