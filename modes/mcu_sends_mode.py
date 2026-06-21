26d7bd0346dde5dca043159df69482a7df2ded1c
bramdebouvere <bram.debouvere@gmail.com>
2026-06-21 17:11:51 +0200

    Add sends mode

--- modes/mcu_sends_mode.py
+++ modes/mcu_sends_mode.py
@@ -1,17 +1,42 @@
 
+from behaviors.jogwheel_behavior import JogWheelBehavior
+from behaviors.mcu_mixer_screen_behavior import McuMixerScreenBehavior
+from behaviors.mixer_encoder_sends_behavior import MixerEncoderSendsBehavior
+from behaviors.mixer_fader_behavior import MixerFaderBehavior
+from behaviors.mixer_meter_behavior import MixerMeterBehavior
+from behaviors.mixer_mute_button_behavior import MixerMuteButtonBehavior
+from behaviors.mixer_rec_button_behavior import MixerRecButtonBehavior
+from behaviors.mixer_select_button_behavior import MixerSelectButtonBehavior
+from behaviors.mixer_solo_button_behavior import MixerSoloButtonBehavior
+from behaviors.namevalue_button_behavior import NameValueButtonBehavior
+from behaviors.track_banking_behavior import TrackBankingBehavior
+from behaviors.mixer_show_bank_in_fl_behavior import MixerShowBankInFLBehavior
+from behaviors.mode_buttons_behavior import ModeButtonsBehavior
 from utilities.track_banking_manager import TrackBankingManager
 from device_hal.mcu_device import McuDevice
 from device_hal import mcu_buttons
 from modes.mcu_base_mode import McuBaseMode
 from utilities.fl_class_import import FlMidiMsg
-from behaviors.mode_buttons_behavior import ModeButtonsBehavior
 
 
 class McuSendsMode(McuBaseMode):
 
     def __init__(self, device: McuDevice, trackBankingManager: TrackBankingManager):
+        screenBehavior = McuMixerScreenBehavior(device, trackBankingManager)
         super().__init__(device, [
-            ModeButtonsBehavior(device, mcu_buttons.Sends)
+            screenBehavior,
+            ModeButtonsBehavior(device, mcu_buttons.Sends),
+            TrackBankingBehavior(device, trackBankingManager),
+            MixerFaderBehavior(device, trackBankingManager),
+            MixerMeterBehavior(device, trackBankingManager),
+            MixerSelectButtonBehavior(device, trackBankingManager),
+            MixerRecButtonBehavior(device, trackBankingManager, screenBehavior),
+            MixerSoloButtonBehavior(device, trackBankingManager),
+            MixerMuteButtonBehavior(device, trackBankingManager),
+            MixerEncoderSendsBehavior(device, trackBankingManager, screenBehavior),
+            MixerShowBankInFLBehavior(device, trackBankingManager),
+            JogWheelBehavior(device, screenBehavior),
+            NameValueButtonBehavior(device, trackBankingManager, screenBehavior)
         ], trackBankingManager)
 
     def OnEnable(self):
